dd9b5ec9b3a5666fdc7197016a36b5fc0a6c1baa
Hassan_ALi <hassan.ali@dailypay.com>
2023-11-16 09:49:30 +0000

    Added css and starting foundation of feedback app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
+import Header from "./components/Header";
 
 function App() {
-    const title = "hello luv"
-    const body = "this is a test"
-    const comments = [
-        {id: 1, text: "this is comment 1"},
-        {id: 2, text: "this is comment 3"},
-        {id: 3, text: "this is comment 3"}
-    ]
-    const loading = false
-    const showComments = true
-    
-    if (loading) return <h1>Loading ....</h1>
-
-    const commentBlock = (
-    <div className="comments">
-    <h3>comments ({comments.length})</h3>
-    <ul>
-        {comments.map((comments, index) =>(
-            <li key={index}>{comments.text}</li>
-        ))}
-    </ul>
-    </div>
-    )
-
      return (
-     <div className="container">
-        <h1>{title.toUpperCase()}</h1>
-        <p>{body}</p>
-
-        {showComments && (commentBlock)
-}
-
-        
-
-     </div>
+        <>
+            <Header />
+            <div className="container">
+                <h1>hello luv</h1>
+            </div>
+        </>
      )
     }
 
